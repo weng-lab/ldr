a50378247f77c512efffc700ab2e07c980006122
hpratt <hpratt111@live.com>
2020-07-30 19:56:31 -0400

    use our own URLs

--- src/ldr/constants/constants.py
+++ src/ldr/constants/constants.py
@@ -2,8 +2,8 @@
 
 HUMAN_SOMATIC_CHROMOSOMES = [ str(x) for x in range(1, 23) ]
 
-BASELINE_WEIGHTS_URL = "https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_weights_hm3_no_MHC.tgz"
-BASELINE_FREQUENCIES_URL = "https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_frq.tgz"
-BASELINE_LD_URL = "https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_baseline_v1.1_ldscores.tgz"
-HAPMAP3_SNPS_URL = "https://data.broadinstitute.org/alkesgroup/LDSCORE/w_hm3.snplist.bz2"
-PLINK_1000G_URL = "https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_plinkfiles.tgz"
+BASELINE_WEIGHTS_URL = "http://gcp.wenglab.org/1000G_Phase3_weights_hm3_no_MHC.tar.gz"
+BASELINE_FREQUENCIES_URL = "http://gcp.wenglab.org/1000G_Phase3_frq.tar.gz"
+BASELINE_LD_URL = "http://gcp.wenglab.org/baseline_v1.1.tar.gz"
+HAPMAP3_SNPS_URL = "http://gcp.wenglab.org/w_hm3.snplist.bz2"
+PLINK_1000G_URL = "http://gcp.wenglab.org/1000G_Phase3_plinkfiles.tgz"
