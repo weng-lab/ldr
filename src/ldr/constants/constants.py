152d49645b563c2b3e430cfe6f45abc5f6b8856a
hpratt <hpratt111@live.com>
2021-07-13 14:58:49 -0400

    move to baseline v2.2

--- src/ldr/constants/constants.py
+++ src/ldr/constants/constants.py
@@ -4,6 +4,6 @@ HUMAN_SOMATIC_CHROMOSOMES = [ str(x) for x in range(1, 23) ]
 
 BASELINE_WEIGHTS_URL = "http://gcp.wenglab.org/1000G_Phase3_weights_hm3_no_MHC.tar.gz"
 BASELINE_FREQUENCIES_URL = "http://gcp.wenglab.org/1000G_Phase3_frq.tar.gz"
-BASELINE_LD_URL = "http://gcp.wenglab.org/baseline_v1.1.tar.gz"
+BASELINE_LD_URL = "http://gcp.wenglab.org/baseline_v2.2.tar.gz"
 HAPMAP3_SNPS_URL = "http://gcp.wenglab.org/w_hm3.snplist.bz2"
 PLINK_1000G_URL = "http://gcp.wenglab.org/1000G_Phase3_plinkfiles.tgz"
